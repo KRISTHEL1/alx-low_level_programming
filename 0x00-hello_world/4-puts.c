634425e9247faa9834ee53568a63b15308680d11
kristhel1 <kristheluvieghara@gmail.com>
2022-10-13 14:10:14 -0700

    c one

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- print in c
+ *
+ * Return: always zero(sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
