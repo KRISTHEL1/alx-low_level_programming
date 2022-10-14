a6a27409a6dab64f2ea048ea64c608708a418542
kristhel1 <kristheluvieghara@gmail.com>
2022-10-14 13:37:36 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- entry
+ *Return:0(success)
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
