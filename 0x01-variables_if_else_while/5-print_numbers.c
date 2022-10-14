0c6fe446d5dfa114906cebcfe862afeb300aa933
kristhel1 <kristheluvieghara@gmail.com>
2022-10-14 13:17:40 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- entry
+ *Return:0(success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
