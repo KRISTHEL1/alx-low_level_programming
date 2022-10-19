11709f493540530863eeccd1a3422253b68cac10
kristhel1 <kristheluvieghara@gmail.com>
2022-10-19 12:24:37 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,18 +8,14 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	last digit = a % 10;
 
 	if (last_digit < 0)
+	{
 		last_digit *= -1;
-	print_last_digit(98);
-
-	print_last_digit(0);
-
-	print_last_digit(-1024);
-
-
+	}
 	_putchar(last_digit + '0');
-
 	return (0);
 }
