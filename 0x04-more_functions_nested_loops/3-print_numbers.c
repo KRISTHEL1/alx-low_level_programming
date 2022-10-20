aed38439494652cafd5a36b5010726f573ee58a7
kristhel1 <kristheluvieghara@gmail.com>
2022-10-20 16:31:46 -0700

    task u

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- print numbers between o to 9
+ * return:0(success)
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+
+	          	_putchar(ch + '0');
+
+	_putchar('\n');
+
+	return (0);
+}
