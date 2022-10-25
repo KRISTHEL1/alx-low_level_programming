2822b167a133f9a7fe5cd170d5eaebb4628a0022
kristhel1 <kristheluvieghara@gmail.com>
2022-10-25 06:05:31 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy- copies the string
+ * @dest: destination
+ * @src: source
+ * Return: 0(success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+	 (src + count) = (dest + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
 
