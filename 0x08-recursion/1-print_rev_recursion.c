a915f51e3666483ffe9c2b1f46f735e9963cb4c9
kristhel1 <kristheluvieghara@gmail.com>
2022-11-11 05:52:57 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _puts_recursion- prints a string in reverse
+ * _print_rev_recursion- prints a string in reverse
  * @s: string
  * Return: Always 0
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
